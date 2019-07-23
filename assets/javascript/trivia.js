2b514528b3358f38cd707637b2f4091caf987ae7
James Appello <jamesappello@gmail.com>
2019-07-23 18:31:33 -0400

    progress2

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,32 +1,56 @@
-//countDwn
-window.onload = function() {
+//Timer INSERT HERE
+$(document).ready(function(){
+
+    var count=30;
+    var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
     
-}
-
-
-
-
-//Trivia Game Start
-function checkFinRes() {
-
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var correct = 0;
-
+    function timer()
+    {
+        count=count-1;
+        if (count <= 0)
+        {
+            clearInterval(counter);
+            return;
+        }
+        
+        $("#seconds").html(count + " seconds left!");
+    }
+    
+    //Trivia Game -- Final submitBtn
+    function checkFinRes() {
+        
+        var question1 = document.quiz.question1.value;
+        var question2 = document.quiz.question2.value;
+        var question3 = document.quiz.question3.value;
+        var correct = 0;
+        
         if (question1 == "1943") {
+            question1 = true;
             correct++;
+        } else {
+            question1 = false;
         }
+        
         if (question2 == "Bill Gates") {
-            correct++;
+            question2 = true;
+            correct++;  
+        } else {
+            question2 = false;
         }
-        if (question3 == "Albany") {
+        
+        if (question3 == "All of the above!") {
+            question3 = true;
             correct++;
+        } else {
+            question3 = false;
         }
+        
 
         document.getElementById("afterSub").style.visibility = "visible";
         
         
-        document.getElementById("amtCorrect").innerHTML = "You got " + correct + " correct!";
-        console.log(correct);
-};
+        $("#amtCorrect").html("You got " + correct + " correct!");
+        
+    };
+});
+    
